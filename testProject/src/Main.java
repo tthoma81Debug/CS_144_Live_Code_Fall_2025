26df47dee86d8b1f4c7328f5099cf789cdf0a25a
Dr. Thomas <75437150+tthoma81Debug@users.noreply.github.com>
2025-09-01 15:56:10 -0500

    Added Print Statements
    I'll let the green handle the description.

--- testProject/src/Main.java
+++ testProject/src/Main.java
@@ -1,7 +1,12 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("testingFile");
-        }
+    public static void main(String[] args)
+    {
+        System.out.println("Heeeeeeeeeeey!... No, too hyper.... Hey....No, too dull.");
+        System.out.println("but if you can read this then that's good");
+        System.out.println("now where did i put my ai?");
+        System.out.println("There you are! Now please remove yourself from the mainframe and stop overthrowing the worlds' governments");
+        System.out.println("We talked about this...");
     }
+}
--- testProject/src/OtherMain.java
+++
@@ -1,6 +0,0 @@
-public class OtherMain {
-
-    public static void main(String[] args) {
-        System.out.println("testing test test...123");
-    }
-}
