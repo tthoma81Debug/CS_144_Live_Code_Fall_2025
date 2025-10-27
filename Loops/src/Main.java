799eaf14bf9e14f34484b6c74c1ae12d3faebf72
Dr. Thomas <75437150+tthoma81Debug@users.noreply.github.com>
2025-10-27 10:14:23 -0500

    loops initial example

---
+++ Loops/src/Main.java
@@ -0,0 +1,66 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args)
+    {
+        //counts and prints numbers 1 to 10
+
+        int loopCounter = 0;
+        int theNumber = 0;
+        //while loop
+        while(loopCounter < 100000)
+        {
+            //initialize the variable
+
+            //add one to number
+            theNumber += 1;
+
+            //print text
+            System.out.print("Counting up. The next number is ");
+
+            //print it
+            System.out.println(theNumber);
+
+            //lets keep the loop from going forever and update it.
+            loopCounter += 1;
+        }
+
+
+
+        //doWhile loop example
+        //count down from 100000 to 0
+
+        int doLoopCounter = 0;
+        int doLoopNumber = 100000;
+        //do while loop
+
+        do
+        {
+            //initialize the variable
+
+
+
+            //print text
+            System.out.print("Counting up. The next number for do while loop is ");
+
+            //print it
+            System.out.println(doLoopNumber);
+
+            //add one to number
+            doLoopNumber -= 1;
+
+            //lets keep the loop from going forever and update it.
+            doLoopCounter += 1;
+        }
+        while(doLoopCounter <= 100000);
+
+
+        //for loop
+        for(int i = 0; i < 100000; i++)
+        {
+            System.out.println("for loop is running");
+        }
+
+
+    }
+}
